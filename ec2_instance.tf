16429b45fc85ea24290ec01f105424089d4bee26
ReeceKay <reecekay42@gmail.com>
2021-07-14 15:32:26 +0100

    First commit

---
+++ ec2_instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    access_key = "AKIATLPTDGMUTKL7DSW7"
+    secret_key = "tfyjD0ln1j7AKSAZfwSS+tgjbyYZ46UtftpFSHTH"
+    region     = "eu-west-2"
+}
+
+resource "aws_instance" "example"{
+    ami = "ami-03ac5a9b225e99b02"
+    instance_type = "t2.micro"
+}
