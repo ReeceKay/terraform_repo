7e03a1f71ae0f5f024bfe01696504df9c795d56f
ReeceKay <reecekay42@gmail.com>
2021-07-14 15:48:58 +0100

    Script to create ec2 instance

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,3 +1,4 @@
+//Creating an EC2 instance
 provider "aws" {
     access_key = "AKIATLPTDGMUTKL7DSW7"
     secret_key = "tfyjD0ln1j7AKSAZfwSS+tgjbyYZ46UtftpFSHTH"
@@ -8,4 +9,3 @@ resource "aws_instance" "example"{
     ami = "ami-03ac5a9b225e99b02"
     instance_type = "t2.micro"
 }
-//hello
